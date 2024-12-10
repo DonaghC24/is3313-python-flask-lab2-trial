4487cdd45fcec2b5f05474039e6fcd7e10fd527b
DonaghC24 <122349863@umail.ucc.ie>
2024-12-10 16:15:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask
+from mymath import power
 
-import mymath
-from mymath import *
-
+# Initialize the Flask application
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    powerString = str(mymath.power(3,8))
-    return f'Hello IS3313. This was edited in Github. The answer is: {powerString}'
+def hello_world():
+    # Calculate 3 raised to the power of 8
+    result = power(3, 8)
+    return (
+        f"<h1>Hello IS3313!</h1>"
+        f"<p>This was edited in GitHub. The answer is: <strong>{result}</strong></p>"
+    )
 
+# Run the application
 if __name__ == '__main__':
-  app.run()
+    app.run(debug=True)
